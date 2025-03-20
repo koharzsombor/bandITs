70faeb89156d3f66aaed7ccbf609e91ff411e552
Kohár Zsombor <zsombor.kohar@gmail.com>
2025-03-20 18:32:22 +0100

    Update SemiFertileTecton.java

--- 6_szkeleton_beadasa/Fungorium/SemiFertileTecton.java
+++ 6_szkeleton_beadasa/Fungorium/SemiFertileTecton.java
@@ -1,9 +1,19 @@
+/**
+ *
+ */
 public class SemiFertileTecton extends Tecton{
+    /**
+     * @param tectonVisitor
+     * @param mushroom
+     */
     @Override
     public void accept(TectonVisitor tectonVisitor, Mushroom mushroom) {
         throw new UnsupportedOperationException("Not implemented");
     }
 
+    /**
+     *
+     */
     @Override
     public void onRoundBegin() {
         throw new UnsupportedOperationException("Not implemented");
