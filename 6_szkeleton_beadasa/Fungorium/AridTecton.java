854486b7bdde6a67a2edb84b660da426cd37de99
Rakos Gergő Máté <gergo_rakos@yahoo.com>
2025-03-22 18:11:32 +0100

    Mushroom and Tecton's commented

--- 6_szkeleton_beadasa/Fungorium/AridTecton.java
+++ 6_szkeleton_beadasa/Fungorium/AridTecton.java
@@ -1,17 +1,26 @@
 /**
- *
+ *  Az AridTecton egy olyan FertileTecton amelyen a rajta levo fonalak
+ *  5 kor/Turn utan felszivodnak/megsemmisulnek
+ *  Kepes interakciokba lepni a MushroomBodyGrowthEvaluator es MushroomBody objektumokkal,
+ *  valamint reagal minden kor/Turn elejen
  */
 public class AridTecton extends FertileTecton implements OnTurnBeginSubscriber {
 
     /**
-     * @param mushroomBodyGrowthEvaluator
-     * @param mushroomBody
+     * Elfogad egy MushroomBodyGrowthEvaluator es egy MushroomBody objektumot,
+     * es vegrehajtja a novekedest vagy az adott MushroomBody torleset aszerint hogy
+     * megvannak a megfelelo korulmenyek-e vagy sem a gombatest novekedesere
+     * @param mushroomBodyGrowthEvaluator - Az evaluator, ennek segitsegevel tud a gombatest
+     *                                    kommunikalni a tektonnal
+     * @param mushroomBody - A gombatest amely szeretne az adott tektonra ranolni
      */
     public void accept(MushroomBodyGrowthEvaluator mushroomBodyGrowthEvaluator, MushroomBody mushroomBody) {
         if (Main.printTrace) {
             System.out.printf("%s\n", Main.objectNames.get(this));
         }
 
+        // Logika a novekedes vagy torlesi folyamat alapjan
+        // Pontosabban ha van legalabb 3 spora, nincs meg gombatest a tektonon es van legalabb egy gombafonal
         if (getSpores().size() < 3 || getMushroomBody() != null || getMycelia().isEmpty()) {
             if (Main.printTrace) {
                 System.out.printf("\t=delete()=> %s", Main.objectNames.get(mushroomBody));
@@ -29,7 +38,8 @@ public class AridTecton extends FertileTecton implements OnTurnBeginSubscriber {
     }
 
     /**
-     *
+     *  A kor/Turn kezdeten hivodo metodus
+     *  Jelenleg nem tartalmaz implementaciot
      */
     @Override
     public void onTurnBegin() {
--- 6_szkeleton_beadasa/Fungorium/Mushroom.java
+++ 6_szkeleton_beadasa/Fungorium/Mushroom.java
@@ -1,14 +1,20 @@
 /**
- *
+ * A Mushroom osztaly egy absztrakt osztaly
+ *  * Definialja a novekedes es torles funkcionalitasait
+ *  * Minden Mushroom tipusu objektum automatikusan egy Turn kezdetenek esemenyere reagal
  */
 public abstract class Mushroom implements OnTurnBeginSubscriber {
     /**
-     *
+     *  Az osztaly egyed torleset megvalosito absztrakt metodus
+     *  Minden alosztalynak implementalnia kell
      */
     public abstract void delete();
 
     /**
-     * @param sporeCount
+     * Celja a metodust meghivo objektum novesztese egy adott tektonra
+     * Minden alosztalynak meg kell hataroznia ennek a funkcionalitasat
+     * @param sporeCount - A Mycelium novesztesekor szukseges, ha > 0 akkor
+     *                   gyorsabban no/kevesebb Turn alatt
      */
     public abstract void grow(int sporeCount);
 }
