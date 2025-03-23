a8c12121766871357ded8ddc07925ac4fed79497
Kohár Zsombor <zsombor.kohar@gmail.com>
2025-03-23 23:48:48 +0100

    Rovar osztály véglegesítése

--- 6_szkeleton_beadasa/Fungorium/Insect.java
+++ 6_szkeleton_beadasa/Fungorium/Insect.java
@@ -1,62 +1,68 @@
 import java.util.*;
 
 /**
- *
+ * Rovar – a gombafonalak mentén mozog (körönként 2 lépést tehet meg), gombafonalakat vág el és spórával táplálkozik.
+ * Ha a gombafonalak eltűnnek alóla, egy véletlenszerűen meghatározott tektonra elmenekül.
+ * Az osztály többek között nyilvántartja, hogy a rovar milyen spóraeffektus alatt áll.
  */
-public class Insect {
+public class Insect implements OnTurnBeginSubscriber {
     /**
-     *
+     * A rovar jelenlegi tartózkodási helye.
      */
     private Tecton location;
 
     /**
-     *
+     * A maximum lépések száma, amit a rovar tehet egy kör alatt.
      */
     private int maxMoves = 2;
 
     /**
-     *
+     * A körben lévő maradék lépéseknek a száma.
      */
     private int remainingMoves = 0;
 
     /**
-     *
+     * A játék alatt elfogyasztott spórák száma.
      */
-    private int sporesEaten;
+    private int sporesEaten = 0;
 
     /**
-     *
+     * A rovar jelenlegi állapotának maradék ideje körökben.
      */
-    private int effectTimer;
+    private int effectTimer = 0;
 
     /**
-     *
+     * A rovar jelenlegi állapota.
      */
     private InsectState state = InsectState.NORMAL;
 
     /**
-     * @return
+     * A rovar jelenlegi tartózkodási helyének getterje.
+     * @return A rovar jelenlegi tartózkodási helye.
      */
     public Tecton getLocation() {
         return location;
     }
 
     /**
-     * @param location
+     * A rovar jelenlegi tartózkodási helyének setterje.
+     * @param location A rovar új tartózkodási helye.
      */
     public void setLocation(Tecton location) {
         this.location = location;
     }
 
     /**
-     * @return
+     * A maximum lépések száma, amit a rovar tehet egy kör alatt getterje.
+     * @return A maximum lépések száma, amit a rovar tehet egy kör alatt.
      */
     public int getMaxMoves() {
         return maxMoves;
     }
 
     /**
-     * @param maxMoves
+     * A maximum lépések száma, amit a rovar tehet egy kör alatt setterje.
+     * @param maxMoves Az új maximum lépések száma, amit a rovar tehet egy kör alatt.
      */
     public void setMaxMoves(int maxMoves) {
         this.maxMoves = maxMoves;
@@ -91,45 +97,49 @@ public class Insect {
     }
 
     /**
-     * @return
+     * A jelenlegi állapotnak a maradék idejének getterje
+     * @return A jelenlegi állapotnak a maradék ideje.
      */
     public int getEffectTimer() {
         return effectTimer;
     }
 
     /**
-     * @param effectTimer
+     * A jelenlegi állapotnak a maradék idejének setterje.
+     * @param effectTimer A jelenlegi állapotnak a maradék idejének új értéke.
      */
     public void setEffectTimer(int effectTimer) {
         this.effectTimer = effectTimer;
     }
 
     /**
-     * @return
+     * A rovar állapotának getterje.
+     * @return A rovar jelenlegi állapota.
      */
     public InsectState getState() {
         return state;
     }
 
     /**
-     * @param state
+     * A rovar állapotának setterjeS.
+     * @param state A rovar új állapota.
      */
     public void setState(InsectState state) {
         this.state = state;
     }
 
     /**
-     *
+     * Elvág egy gombafonalat a jelenlegi tartózkodási helyén.
      */
     public void cutMycelium() {
-        if(getRemainingMoves()>0) {
+        if(getRemainingMoves() > 0 && getState() != InsectState.CANNOT_CUT) {
             location.cutMycelium();
             setRemainingMoves(getRemainingMoves()-1);
         }
     }
 
     /**
-     *
+     * A rovar megeszi azt a spórát,
      */
     public void eatSpore() {
         if (Main.printTrace) {
@@ -141,7 +151,8 @@ public class Insect {
     }
 
     /**
-     * @param target
+     * A rovar elmozdul a megadott céltektonra, ha van elég lépése a rovarnak.
+     * @param target A cél tekton.
      */
     public void move(Tecton target) {
         if(Main.printTrace) {
@@ -155,31 +166,35 @@ public class Insect {
     }
 
     /**
-     *
+     * A rovar lassú állapotba kerül.
      */
     public void beSlow() {
         setState(InsectState.SLOW);
+        setMaxMoves(1);
     }
 
     /**
-     *
+     * A rovar gyors állapotba kerül.
      */
     public void beFast() {
         setState(InsectState.FAST);
+        setMaxMoves(3);
     }
 
     /**
-     *
+     * A rovar olyan állapotba kerül, amitől nem tudja elvágni a gombafonalakat.
      */
     public void preventCut() {
         setState(InsectState.CANNOT_CUT);
     }
 
     /**
-     *
+     * A rovar bénult állapotba kerül.
      */
     public void beStunned() {
         setState(InsectState.STUN);
+        setMaxMoves(0);
+        setMaxMoves(0);
     }
 
     /**
@@ -257,5 +272,23 @@ public class Insect {
 
             selectedTecton.addOccupant(this);
         }
+    }
+
+    /**
+     * A körök kezdetén a jelenlegi állapot visszaszámlálóját csökkenti és ha eléri a 0-át visszatér normál állapotba.
+     * Ezenfelül visszaállítja a maradék lépéseket a maximumra.
+     */
+    @Override
+    public void onTurnBegin() {
+        if (effectTimer > 0) {
+            effectTimer--;
+
+            if (effectTimer <= 0) {
+                setState(InsectState.NORMAL);
+                setMaxMoves(2);
+            }
+        }
+
+        remainingMoves = maxMoves;
     }
 }
