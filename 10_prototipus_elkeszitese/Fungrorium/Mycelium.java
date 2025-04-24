3f1fe117ae0114496f1687c958c6027b93834634
G-gergo <guzmicsgergo@gmail.com>
2025-04-24 21:37:29 +0200

    Félkész Mycelium

--- 10_prototipus_elkeszitese/Fungrorium/Mycelium.java
+++ 10_prototipus_elkeszitese/Fungrorium/Mycelium.java
@@ -1,2 +1,14 @@
-public interface Mycelium {
+public interface Mycelium extends MyceliumControl, MyceliumView, Mushroom{
+    /**
+     * A gombafonál elvágódik, ezzel szól a többi gombafonálnak, hogy nézzék meg, hogy hozzá vannak-e csatlakoztatva
+     * egy gombatesthez.
+     */
+    public void cutImmediate();
+
+    /**
+     * Beállítja a deathTimer-t 2-re
+     */
+    public void cutWithDelay();
+
+
 }
---
+++ 10_prototipus_elkeszitese/Fungrorium/MyceliumImpl.java
@@ -0,0 +1,166 @@
+/**
+ * Gombafonál – olyan gombarész, amelyre gombatest nőhet.
+ * Elvágása esetén a gombafonál akkor is fennmarad, ha olyan tektonon halad át, amelyen található gombatest.
+ * Új gombatestet olyan gombafonál növeszthet, amely összeköttetésben áll az eredeti gombatestével.
+ */
+
+import java.util.*;
+
+public class MyceliumImpl implements Mycelium{
+
+    /**
+     * Azt ábrázolja, hogy a gombafonál éppen növekedés alatt van-e.
+     */
+    protected boolean growing = false;
+
+    /**
+     * A tekton, ahol a gombafonál elhelyezkedik.
+     */
+    protected Tecton location;
+
+    /**
+     * Az idő, ami alatt a gombafonál megnő.
+     */
+    protected int growTimer = 0;
+
+    /**
+     * Az idő, ami mulva elszakad a fonál.
+     */
+    protected int deathTimer= -1;
+
+    /**
+     * Létrehoz egy üres gombafonalat, ami nincs rajta egy tektonon.
+     * Csak a játékmező létrehozására szabad használni.
+     */
+    public MyceliumImpl() {}
+
+    /**
+     * Létrehoz egy gombafonalat az adott tektonra, ha a tekton befogadja, akkor létrejön a tektonon, ha nem megsemmisül.
+     * Azért, hogy a visitor egyértelműen el tudja, dönteni, hogy milyen tekton-t kell meglátogatnia,
+     * mindegyik tekton típusra kell egy külön konstruktor.
+     * @param location A tekton, ahová a gombafonál nőni szeretne.
+     * @param name A konzolra kiírt objektum név. Csak a szkeleton programban szükséges.
+     */
+    public MyceliumImpl(FertileTecton location, String name) {
+        this.location = location;
+
+        MyceliumGrowthEvaluator myceliumGrowthEvaluator = new MyceliumGrowthEvaluator(this);
+        myceliumGrowthEvaluator.visit(location);
+    }
+
+    public MyceliumImpl(SemiFertileTecton location, String name) {
+        this.location = location;
+
+        MyceliumGrowthEvaluator myceliumGrowthEvaluator = new MyceliumGrowthEvaluator(this);
+        myceliumGrowthEvaluator.visit(location);
+    }
+
+    /**
+     * A függvény kitörli a gombafonalat.
+     */
+    @Override
+    public void delete() {
+        location=null;
+    }
+
+    /**
+     * A growing attribútum getterje.
+     * @return Éppen növekedés alatt van-e a gombafonál.
+     */
+    @Override
+    public boolean isGrowing() {
+        return growing;
+    }
+
+    /**
+     * A growing attribútum setterje.
+     * @param growing A gombafonál növekedés alatt lesz-e.
+     */
+    @Override
+    public void setGrowing(boolean growing) {
+        this.growing = growing;
+    }
+
+    /**
+     * A függvény hívására a gombafonál megkezdi a növekedést.
+     * @param sporeCount A tektonon lévő spórák száma, ez alapján dönti el, hogy mennyi idő lesz növekedni.
+     */
+    @Override
+    public void grow(int sporeCount) {
+        growing = true;
+        growTimer = sporeCount > 0 ? 1 : 2;
+    }
+
+    /**
+     * A location attribútum getterje.
+     * @return A tekton, ahol a gombafonál van.
+     */
+    @Override
+    public Tecton getLocation() {
+        return location;
+    }
+
+    /**
+     * A location attribútum setterje.
+     * @param location A tekton, ahová a gombafonál kerül.
+     */
+    @Override
+    public void setLocation(Tecton location) {
+        this.location = location;
+    }
+
+    /**
+     * A gombafonál elvágódik, ezzel szól a többi gombafonálnak, hogy nézzék meg, hogy hozzá vannak-e csatlakoztatva
+     * egy gombatesthez.
+     */
+    @Override
+    public void cutImmediate(){
+        location.checkNeighborMyceliaSustain();
+        if(location.getMycelia().isEmpty()){
+            for(Insect i: location.getOccupants()){
+                i.runAway();
+            }
+        }
+        this.delete();
+    }
+
+    /**
+     * Beállítja a deathTimer-t 2-re. A rovar fonalvágása során lesz ez releváns
+     */
+    @Override
+    public void cutWithDelay(){
+        deathTimer=2;
+    }
+
+    /**
+     * Amikor kör kezdődik, a növekedési idő csökken, és ha az időzítő lejárt, akkor befejezi a növekedést.
+     */
+    @Override
+    public void onTurnBegin() {
+        if (growing) {
+            growTimer--;
+            growing = growTimer <= 0;
+            return;
+        }
+
+        if(deathTimer==0){
+            this.cutImmediate();
+            return;
+        }
+
+        if(deathTimer>0){
+            deathTimer--;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(); sb.append(": Mycelium\n");
+        sb.append("\t"); sb.append("growing boolean = "); sb.append(growing); sb.append("\n");
+        sb.append("\t"); sb.append("location Tecton = "); sb.append(location); sb.append("\n");
+        sb.append("\t"); sb.append("growTimer int = "); sb.append(growTimer);
+        sb.append("\t"); sb.append("deathTimer int = "); sb.append(deathTimer);
+        return sb.toString();
+    }
+}
