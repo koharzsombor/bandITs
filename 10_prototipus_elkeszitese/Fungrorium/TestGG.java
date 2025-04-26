052fd26d30d080e82b6ecd50daea5e8f6fb8c3ff
G-gergo <guzmicsgergo@gmail.com>
2025-04-27 01:00:08 +0200

    Teszt 1-8 ig

--- 10_prototipus_elkeszitese/Fungrorium/TestGG.java
+++ 10_prototipus_elkeszitese/Fungrorium/TestGG.java
@@ -134,10 +134,10 @@ public class TestBJ {
         List<String> output = traceablePrinter.readHistroy();       //Troy!!
         Assertions.assertTrue(output.get(0).equals(test1_ft2));
         Assertions.assertTrue(output.get(1).equals(test1_m1));
-        Assertions.assertTrue(output.get(0).equals(test1_ft1));
-        Assertions.assertTrue(output.get(0).equals(test1_ft2_2));
-        Assertions.assertTrue(output.get(1).equals(test1_mb1));
-        Assertions.assertTrue(output.get(1).equals(test1_m1_2));
+        Assertions.assertTrue(output.get(2).equals(test1_ft1));
+        Assertions.assertTrue(output.get(3).equals(test1_ft2_2));
+        Assertions.assertTrue(output.get(4).equals(test1_mb1));
+        Assertions.assertTrue(output.get(5).equals(test1_m1_2));
     }
 
     //Teszt2: Gombafonál sikeres gyors növesztése gombatestből FertileTectonra
@@ -192,9 +192,9 @@ public class TestBJ {
 
         List<String> output = traceablePrinter.readHistroy();
         Assertions.assertTrue(output.get(0).equals(test2_ft1));
-        Assertions.assertTrue(output.get(0).equals(test2_ft2));
-        Assertions.assertTrue(output.get(1).equals(test2_mb1));
-        Assertions.assertTrue(output.get(1).equals(test2_m1));
+        Assertions.assertTrue(output.get(1).equals(test2_ft2));
+        Assertions.assertTrue(output.get(2).equals(test2_mb1));
+        Assertions.assertTrue(output.get(3).equals(test2_m1));
     }
 
     //Teszt3: Gombafonál sikertelen növesztése gombatestből, olyan FertileTectonra, ahol már van gombafonál
@@ -250,8 +250,8 @@ public class TestBJ {
         List<String> output = traceablePrinter.readHistroy();
         Assertions.assertTrue(output.get(0).equals(test3_ft1));
         Assertions.assertTrue(output.get(1).equals(test3_ft2));
-        Assertions.assertTrue(output.get(0).equals(test3_mb1));
-        Assertions.assertTrue(output.get(0).equals(test3_m1));
+        Assertions.assertTrue(output.get(2).equals(test3_mb1));
+        Assertions.assertTrue(output.get(3).equals(test3_m1));
     }
 
     //Teszt4: Gombafonál sikertelen növesztése gombatestből, olyan FertileTectonra , ami a növést kezdeményező gombatest tektonjával nem közvetlenül szomszédos.
@@ -296,7 +296,7 @@ public class TestBJ {
         List<String> output = traceablePrinter.readHistroy();
         Assertions.assertTrue(output.get(0).equals(test4_ft1));
         Assertions.assertTrue(output.get(1).equals(test4_ft2));
-        Assertions.assertTrue(output.get(0).equals(test4_mb1));
+        Assertions.assertTrue(output.get(2).equals(test4_mb1));
     }
 
     //Teszt5: Húsevő fonál általi rovarevés és gombatest növesztés
@@ -327,6 +327,139 @@ public class TestBJ {
 
         List<String> output = traceablePrinter.readHistroy();
         Assertions.assertTrue(output.get(0).equals(test5_ft1));
-        Assertions.assertTrue(output.get(0).equals(test5_mb_ft1));
+        Assertions.assertTrue(output.get(1).equals(test5_mb_ft1));
     }
+
+    //Teszt6: Gombafonál elhalása AridTectonon
+    private static final String test6_Path = "Fungrorium/TestInputs/GGTests/test6.txt";
+    private static final String test6_at1 = "at1: FertileTecon\n" +
+            "\tbreakTimer int = 1\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 1\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = null\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t}\n";
+
+    @Test
+    public void test6() {
+        commandReader.bufferFile(test6_Path);
+        commandReader.readAllBufferedCommands();
+
+        List<String> output = traceablePrinter.readHistroy();
+        Assertions.assertTrue(output.get(0).equals(test6_at1));
+    }
+
+    //Teszt7: Rovarász megpróbál a rovarral műveletet (evés, vágás, mozgás) végrehajtani, amikor már nincs több művelete
+    private static final String test7_Path = "Fungrorium/TestInputs/GGTests/test7.txt";
+    private static final String test7_ft3 = "ft3: FertileTecon\n" +
+            "\tbreakTimer int = 4\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t\tf2\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 1\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t\tspeeds1\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = null\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t\tm3\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t\ti1\n" +
+            "\t}\n";
+    private static final String test7_m3 = "m3: Mycelium\n" +
+            "\tgrowing boolean = false\n" +
+            "\tlocation Tecton = f3\n" +
+            "\tgrowTimer int = 0\n" +
+            "\tdeathTimer int = -1\n";
+    private static final String test7_i1 = "i1: Insect\n" +
+            "\tlocation = ft3\n" +
+            "\tmaxMoves = 2\n" +
+            "\tremainingMoves = 0\n" +
+            "\tsporesEaten = 0\n" +
+            "\teffectTimer = 0\n" +
+            "\tstate = NORMAL\n";
+
+    @Test
+    public void test7() {
+        commandReader.bufferFile(test7_Path);
+        commandReader.readAllBufferedCommands();
+
+        List<String> output = traceablePrinter.readHistroy();
+        Assertions.assertTrue(output.get(0).equals(test7_ft3));
+        Assertions.assertTrue(output.get(1).equals(test7_m3));
+        Assertions.assertTrue(output.get(2).equals(test7_i1));
+    }
+
+    //Teszt8: Gombász megpróbál a körében olyan műveletet végezni, amire már nincs lehetősége
+    private static final String test8_Path = "Fungrorium/TestInputs/GGTests/test8.txt";
+    private static final String test8_ft1 = "ft1: FertileTecon\n" +
+            "\tbreakTimer int = 3\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t\tf3\n" +
+            "\t\tf4\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 1\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = mb1\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t}\n";
+    private static final String test8_ft2 = "ft2: FertileTecon\n" +
+            "\tbreakTimer int = 3\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t\tf3\n" +
+            "\t\tf4\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 1\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = mb2\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t}\n";
+    private static final String test8_ft3 = "ft3: FertileTecon\n" +
+            "\tbreakTimer int = 3\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t\tf1\n" +
+            "\t\tf2\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 1\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t\tspeeds1\n" +
+            "\t\tspeeds5\n" +
+            "\t\tspeeds6\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = mb3\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t\tm2\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t}\n";
+    private static final String test8_mlt1 = "mlt1: MultiLayeredTecon\n" +
+            "\tbreakTimer int = 3\n" +
+            "\tneighbours List<Tecton>  = {\n" +
+            "\t\tf1\n" +
+            "\t\tf2\n" +
+            "\t}\n" +
+            "\tmyceliumCapacity int = 3\n" +
+            "\tspores Queue<Spore> = {\n" +
+            "\t\tspeeds2\n" +
+            "\t\tspeeds3\n" +
+            "\t\tspeeds4\n" +
+            "\t}\n" +
+            "\tmushroomBody MushroomBody = mb4\n" +
+            "\tmycelia Queue<Mycelium> = {\n" +
+            "\t\tm1\n" +
+            "\t}\n" +
+            "\toccupants List<Insect> = {\n" +
+            "\t}\n";
 }
