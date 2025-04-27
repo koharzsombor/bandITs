0c09ed0143d631a14d7b7e459a4297d59e87d2c7
Kohár Zsombor <zsombor.kohar@gmail.com>
2025-04-27 12:51:15 +0200

    magicfix

--- 10_prototipus_elkeszitese/Fungrorium/SemiFertileTectonImpl.java
+++ 10_prototipus_elkeszitese/Fungrorium/SemiFertileTectonImpl.java
@@ -15,7 +15,7 @@ public class SemiFertileTectonImpl extends TectonImpl {
 
     @Override
     public void accept(MyceliumGrowthEvaluator myceliumGrowthEvaluator, Mycelium mycelium) {
-        if (getMycelia().size() >= getMyceliaCapacity()) {
+        if (getMycelia().size() >= getMyceliaCapacity() || neighboursWithMycelia().isEmpty()) {
             mycelium.delete();
             return;
         }
